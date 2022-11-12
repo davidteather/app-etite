70beb2e49469b9573243193c911d13a085c25508
David Teather <34144122+davidteather@users.noreply.github.com>
2022-11-12 16:49:43 -0600

    Restaurant Wrapper

--- app-etite/src/App.js
+++ app-etite/src/App.js
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import Filters from './Filters';
 import ImageSlides from './ImageSlides';
+import RestaurantData from './data.json';
+import RestaurantWrapper from './RestaurantWrapper';
 
+function getRandomRestaurant() {
+  var RestaurantKeys = Object.keys(RestaurantData);
+  var randomKey = RestaurantKeys[Math.floor(Math.random() * RestaurantKeys.length)];
+  return RestaurantData[randomKey];
+}
 
 function App() {
   return (
     <div className="App">
       <Filters/>
+      <RestaurantWrapper restaurant={getRandomRestaurant()}/>
       <ImageSlides/>
+      {console.log(RestaurantData)}
+      {Object.keys(RestaurantData).forEach(key => {
+        return <div>RestaurantData[key].place_id</div>
+      })}
     </div>
   );
 }
---
+++ app-etite/src/RestaurantWrapper.js
@@ -0,0 +1,8 @@
+export default function RestaurantWrapper(props) {
+  var resturant = props.restaurant;
+  return (
+    <>
+      {resturant.title}
+    </>
+  );
+};
