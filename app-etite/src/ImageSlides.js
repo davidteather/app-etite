f369eae6b1b23f28826f4f450e0b2b7d20b68b3d
Sarthak Agrawal <sarthavader@gmail.com>
2022-11-12 14:25:25 -0600

    seperated into components

---
+++ app-etite/src/ImageSlides.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function ImageSlides() {
+  return (
+    <div className="image-slides">
+      <h1>Image slides!</h1>
+    </div>
+  );
+}
+
+export default ImageSlides;
