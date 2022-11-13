319116bc7433b5e56bbb609c733259e9ee42b422
David Teather <34144122+davidteather@users.noreply.github.com>
2022-11-12 18:56:21 -0600

    Fix retry issue

--- app-etite/src/components/ImageSlides.js
+++ app-etite/src/components/ImageSlides.js
@@ -4,16 +4,21 @@ import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io"
 
 
 function ImageSlides(props) {
-  // TODO:
-  function retryRequest (url) {
-    fetch(url, {mode: "no-cors"}).then(r=>r.blob()).then(d=> this.src=window.URL.createObjectURL(d));
+  function retryRequest(url) {
+    if (document.getElementById('image_loader') !== null) {
+      document.getElementById('image_loader').src="/ffffff.png"
+      setTimeout(() => {document.getElementById('image_loader').src=url}, 10)
+    }
   }
+
   return (
     <div className="image-slides">
       <div className="image-container">
         <img
+          id="image_loader"
           src={props.restaurant.thumbnail}
-          onError={retryRequest(props.restaurant.thumbnail)}
+          //onError={retryRequest(props.restaurant.thumbnail)}
+          onError={() => retryRequest(props.restaurant.thumbnail)}
           alt={props.restaurant.description}
           onClick={props.newGenerator}
         />
