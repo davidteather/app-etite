984c9c04ec9a9c3e06340dabfb8a164b1c81ce57
jkintana <jkintana@protonmail.com>
2022-11-13 11:11:35 -0600

    description fix

--- app-etite/src/components/RestaurantDescription.js
+++ app-etite/src/components/RestaurantDescription.js
@@ -16,16 +16,38 @@ function RestaurantDescription(props) {
   return (
     <div className='description'>
       <div key={props.restaurant.title} className='current'>
-        <h2>{props.restaurant.title} {props.restaurant.price}</h2>
-        <Rating initialValue={Math.round(oldRestaurant.rating)} isHalf={true} readonly />
-        {/* <h4>{props.restaurant.address}</h4>
-        <a href={props.restaurant.website}><p>Website</p></a> */}
+        <h2>
+          {props.restaurant.title}
+        </h2>
+        <h3>
+          <Rating initialValue={Math.round(props.restaurant.rating)} isHalf={true} readonly />
+          {/* {props.restaurant.reviews} reviews */}
+        </h3>
+        <h4>
+          {/* {props.restaurant.price} */}
+          {props.restaurant.type}
+        </h4>
+        <h5>
+          {props.restaurant.address}
+        </h5>
+        <a href={props.restaurant.website}><p>Website</p></a>
       </div>
       <div key={props.restaurant.address} className='old'>
-        <h2>{oldRestaurant.title} {oldRestaurant.price}</h2>
-        <Rating initialValue={Math.round(oldRestaurant.rating)} allowFraction readonly />
-        {/* <h4>{oldRestaurant.address}</h4>
-        <a href={oldRestaurant.website}><p>Website</p></a> */}
+      <h2>
+          {oldRestaurant.title}
+        </h2>
+        <h3>
+          <Rating initialValue={Math.round(oldRestaurant.rating)} isHalf={true} readonly />
+          {/* {props.restaurant.reviews} reviews */}
+        </h3>
+        <h4>
+          {/* {props.restaurant.price} */}
+          {oldRestaurant.type}
+        </h4>
+        <h5>
+          {oldRestaurant.address}
+        </h5>
+        <a href={oldRestaurant.website}><p>Website</p></a>
       </div>
     </div>
   );
