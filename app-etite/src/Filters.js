082f618151e5e2bfd8b8e2d5d81153db1f54bff8
jkintana <jkintana@protonmail.com>
2022-11-12 20:28:02 -0600

    Sarthak's sliders

--- app-etite/src/Filters.js
+++ app-etite/src/Filters.js
@@ -1,27 +1,52 @@
 import React, { useState } from 'react';
 import { AiOutlineArrowDown } from 'react-icons/ai';
 import './Filters.css';
+import ReactSlider from "react-slider";
+
+function Filter(props) {
+  const [data, setData] = useState(props.def)
+  return (
+    <div class="slider">
+      <input
+        type="range"
+        min={props.min}
+        max={props.max}
+        defaultValue={props.def}
+        class="slider"
+        id="myRange"
+        onChange={(e, val) => props.setData(e.target.value)}
+      />
+    </div>);
+}
 
 function Filters() {
   const [visible, setVisible] = useState(false);
+  const [stars, setStars] = useState(1);
 
   const flipVisible = () => {
     setVisible(!visible)
   }
 
   return (
-    <div className={"filters " + (visible? "active" : "inactive")}>
-      <ul>
-        <li>
-          Option 1
-        </li>
-        <li>
-          Option 2
-        </li>
-        <li>
-          Option 3
-        </li>
-      </ul>
+    <div className={"dropdown " + (visible ? "active" : "inactive")}>
+      <div className="filterContainer">
+        <div class="filter">
+          <Filter min="1" max="5" def="1" setData={setStars} />
+          Minimum Stars: {stars}
+        </div>
+        <div class="filter">
+          <Filter min="1" max="5" def="1" setData={setStars} />
+          Minimum Stars: {stars}
+        </div>
+        <div class="filter">
+          <Filter min="1" max="5" def="1" setData={setStars} />
+          Minimum Stars: {stars}
+        </div>
+        <div class="filter">
+          <Filter min="1" max="5" def="1" setData={setStars} />
+          Minimum Stars: {stars}
+        </div>
+      </div>
       <button onClick={() => { flipVisible() }}>
         <AiOutlineArrowDown />
       </button>
