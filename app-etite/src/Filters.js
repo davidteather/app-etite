f138246c36f7eeeff09ad0edfc9a111b1c4c724d
jkintana <jkintana@protonmail.com>
2022-11-12 18:57:18 -0600

    basic functionality for dropdown menu

--- app-etite/src/Filters.js
+++ app-etite/src/Filters.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { AiOutlineArrowDown } from 'react-icons/ai';
+import './Filters.css';
 
 function Filters() {
+  const [visible, setVisible] = useState(false);
+
+  const flipVisible = () => {
+    setVisible(!visible)
+  }
+
   return (
-    <div className="filters">
-      <h1>Filters!</h1>
+    <div className={"filters " + (visible? "active" : "inactive")}>
+      <ul>
+        <li>
+          Option 1
+        </li>
+        <li>
+          Option 2
+        </li>
+        <li>
+          Option 3
+        </li>
+      </ul>
+      <button onClick={() => { flipVisible() }}>
+        <AiOutlineArrowDown />
+      </button>
     </div>
   );
 }
