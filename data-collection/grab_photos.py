a4363afd87034ece63b8a8f70683cf8a9659d97f
David Teather <34144122+davidteather@users.noreply.github.com>
2022-11-13 10:00:29 -0600

    Convert to food images

--- data-collection/grab_photos.py
+++ data-collection/grab_photos.py
@@ -8,13 +8,22 @@ with open('data.json') as f:
     data = json.load(f)
 
 from serpapi import GoogleSearch
+import os
+
+seen_before = set()
+for file in os.listdir("image_data"):
+    if file.endswith(".json"):
+        seen_before.add(file.replace(".json", ""))
 
 for place_id, restaurant in data.items():
+    if restaurant['data_id'] in seen_before:
+        continue
     data_id = restaurant['data_id']
     params = {
         "engine": "google_maps_photos",
         "data_id": data_id,
-        "api_key": config['SERPAPI_KEY']
+        "api_key": config['SERPAPI_KEY'],
+        "category_id": "CgIYIA" # Food
     }
 
     search = GoogleSearch(params)
@@ -25,5 +34,3 @@ for place_id, restaurant in data.items():
     # Writing to sample.json
     with open(f"image_data/{data_id}.json", "w+", encoding='utf-8') as outfile:
         outfile.write(json_object)
-
-    exit()
