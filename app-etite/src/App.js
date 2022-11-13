7e7af87e7e39f16517a2755c12c25cf9babfe23c
Sarthak Agrawal <sarthavader@gmail.com>
2022-11-12 20:29:38 -0600

    added info

--- app-etite/src/App.js
+++ app-etite/src/App.js
@@ -7,21 +7,25 @@ import RestaurantWrapper from './RestaurantWrapper';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
 
-function getRandomRestaurant() {
+function getRandomRestaurant(location) {
   var RestaurantKeys = Object.keys(RestaurantData);
   var randomKey = RestaurantKeys[Math.floor(Math.random() * RestaurantKeys.length)];
   return RestaurantData[randomKey];
 }
 
 function App() {
+  const location = navigator.geolocation.getCurrentPosition(function (pos) {
+    console.log(pos)
+  })
+
   function newRestaurantGenerator() {
-    setRestaurant(getRandomRestaurant());
+    setRestaurant(getRandomRestaurant(location));
   }
-  const [restaurant, setRestaurant] = useState(getRandomRestaurant());
+  const [restaurant, setRestaurant] = useState(getRandomRestaurant(location));
   return (
     <div className="App">
-      <Filters/>
-      <RestaurantWrapper restaurant={restaurant} newGenerator={newRestaurantGenerator}/>
+      <Filters />
+      <RestaurantWrapper restaurant={restaurant} newGenerator={newRestaurantGenerator} />
     </div>
   );
 }
--- app-etite/src/components/ImageSlides.js
+++ app-etite/src/components/ImageSlides.js
@@ -1,19 +1,54 @@
 import React, { useState } from 'react';
 import "../styles/imageslides.css"
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io"
+import { FcInfo } from "react-icons/fc"
 
+function DetailsMenu(props) {
+  const [open, setOpen] = useState(false);
+
+  const menu = () => {
+    setOpen(!open);
+  }
+  if (open) {
+    return (
+      <div class="details-menu">
+        <button onClick={menu}><FcInfo /></button>
+
+      </div>
+    )
+  } else {
+    return (
+      <>
+        <div class="details-menu">
+          <button onClick={menu}><FcInfo /></button>
+
+        </div>
+        <div class="open">
+          <h2>{props.restaurant.title} {props.restaurant.price}</h2>
+          <h4>Stars: {props.restaurant.rating}</h4>
+          <h4>{props.restaurant.address}</h4>
+          <a href={props.restaurant.website}><p>Website</p></a>
+        </div>
+
+      </>
+    )
+  }
+}
 
 function ImageSlides(props) {
   return (
-    <div className="image-slides">
+    <><div className="image-slides">
       <div class="image-container">
         <img
           src={props.restaurant.thumbnail}
           alt="img"
           onClick={props.newGenerator}
         />
+        <DetailsMenu restaurant={props.restaurant}/>
       </div>
     </div>
+    </>
+
+
   );
 }
 
