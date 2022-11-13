4e4e4b0ae3a05b03e3343a7da6f46696cafef262
Sarthak Agrawal <sarthavader@gmail.com>
2022-11-13 10:38:20 -0600

    stars

--- app-etite/src/components/RestaurantDescription.js
+++ app-etite/src/components/RestaurantDescription.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 import '../styles/RestaurantDescription.css';
+import { Rating } from 'react-simple-star-rating'
+
 
 function RestaurantDescription(props) {
 
@@ -15,15 +17,15 @@ function RestaurantDescription(props) {
     <div className='description'>
       <div key={props.restaurant.title} className='current'>
         <h2>{props.restaurant.title} {props.restaurant.price}</h2>
-        <h4>Stars: {props.restaurant.rating}</h4>
-        <h4>{props.restaurant.address}</h4>
-        <a href={props.restaurant.website}><p>Website</p></a>
+        <Rating initialValue={oldRestaurant.rating} isHalf={true} readonly />
+        {/* <h4>{props.restaurant.address}</h4>
+        <a href={props.restaurant.website}><p>Website</p></a> */}
       </div>
       <div key={props.restaurant.address} className='old'>
         <h2>{oldRestaurant.title} {oldRestaurant.price}</h2>
-        <h4>Stars: {oldRestaurant.rating}</h4>
-        <h4>{oldRestaurant.address}</h4>
-        <a href={oldRestaurant.website}><p>Website</p></a>
+        <Rating initialValue={oldRestaurant.rating} allowFraction readonly />
+        {/* <h4>{oldRestaurant.address}</h4>
+        <a href={oldRestaurant.website}><p>Website</p></a> */}
       </div>
     </div>
   );
